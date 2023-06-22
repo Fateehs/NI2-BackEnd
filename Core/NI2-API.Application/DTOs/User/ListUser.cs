cb35637d1ab27426147acdf7157f59653fec9d20
Fateeh <benzer1010@gmail.com>
2023-06-22 16:49:26 +0300

    DTO's created, Properties added

---
+++ Core/NI2-API.Application/DTOs/User/ListUser.cs
@@ -0,0 +1,10 @@
+﻿namespace NI2_API.Application.DTOs.User
+{
+    public class ListUser
+    {
+        public string Id { get; set; }
+        public string SteamId { get; set; }
+        public string Username { get; set; }
+
+    }
+}
