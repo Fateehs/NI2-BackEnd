be698395c46d778dbd115424cc88e05193610e9b
Fateeh <benzer1010@gmail.com>
2023-06-22 16:49:14 +0300

    Token Handler created

---
+++ Core/NI2-API.Application/Abstractions/Token/ITokenHandler.cs
@@ -0,0 +1,9 @@
+﻿using NI2_API.Domain.Entities.Identity;
+
+namespace NI2_API.Application.Abstractions.Token
+{
+    public interface ITokenHandler
+    {
+        DTOs.Token CreateAccessToken(int second, AppUser appUser);
+    }
+}
---
+++ Infrastructure/NI2-API.Infrastructure/Services/Token/TokenHandler.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NI2_API.Application.Abstractions.Token;
+using NI2_API.Domain.Entities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NI2_API.Infrastructure.Services.Token
+{
+    public class TokenHandler : ITokenHandler
+    {
+        readonly IConfiguration _configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Application.DTOs.Token CreateAccessToken(int second, AppUser appUser)
+        {
+            Application.DTOs.Token token = new();
+
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_configuration["Token:SecurityKey"]));
+
+            SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.UtcNow.AddSeconds(second);
+
+            JwtSecurityToken jwtSecurityToken = new(
+                audience: _configuration["Token:Audience"],
+                issuer: _configuration["Token:Issuer"],
+                expires: token.Expiration,
+                notBefore: DateTime.UtcNow,
+                signingCredentials: signingCredentials,
+                claims: new List<Claim> { new(ClaimTypes.Name, appUser.UserName) }
+                );
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+            token.AccessToken = jwtSecurityTokenHandler.WriteToken(jwtSecurityToken);
+
+            token.RefreshToken = CreateRefreshToken();
+
+            return token;
+        }
+
+        public string CreateRefreshToken()
+        {
+            byte[] number = new byte[32];
+            using RandomNumberGenerator random = RandomNumberGenerator.Create();
+            random.GetBytes(number);
+            return Convert.ToBase64String(number);
+        }
+    }
+}
