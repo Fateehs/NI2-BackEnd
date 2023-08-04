fe158e726aa039d7d165fded35a1bdfad3e84f2a
Fateeh <benzer1010@gmail.com>
2023-08-04 14:02:02 +0300

    Entity modified, Property updated

--- Core/NI2-API.Domain/Entities/Identity/AppUser.cs
+++ Core/NI2-API.Domain/Entities/Identity/AppUser.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using NI2_API.Domain.Entities.User;
 
 namespace NI2_API.Domain.Entities.Identity
 {
     public class AppUser : IdentityUser<string>
     {
+        public ICollection<Character> Characters { get; } = new List<Character>(); // Each user can have many chars
     }
 }
